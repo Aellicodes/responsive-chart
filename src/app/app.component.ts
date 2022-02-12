b4047cc4576457c8ee89e92fe7825b7819d36875
Aelli codes <code.essence.mail@gmail.com>
2022-02-12 15:50:08 +0100

    responsive chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { colors } from './colors';
+import { Component, ViewChild } from '@angular/core';
+import { BaseChartDirective, Label } from 'ng2-charts';
+import { ChartDataSets, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'responsive-chart';
+  @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
+  public chartLabels: Label[] = ['El1', 'El2', 'El3', 'El4'];
+  public chartData: ChartDataSets[] = [
+    {data: [26,	21, 35, 42], label: 'Data 1', backgroundColor: colors.chartColor1,
+    borderColor: colors.chartColor1, pointBackgroundColor: colors.darkColor},
+    {data: [70, 55,	82, 65], label: 'Data 2', backgroundColor: colors.chartColor2,
+    borderColor: colors.chartColor2, pointBackgroundColor: colors.darkColor}
+  ];
+  public chartOptions: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          color: colors.lightColor
+        },
+        ticks: {
+          fontColor: colors.lightColor
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          color: colors.lightColor
+        },
+        ticks: {
+          beginAtZero: true,
+          fontColor: colors.lightColor
+        }
+      }]
+    },
+    legend: {
+      labels: {
+        fontColor: colors.lightColor
+      }
+    }
+  };
 }
---
+++ src/app/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  darkColor: '#7b6079',
+  lightColor: '#ffe9d6',
+  chartColor1: '#de8971',
+  chartColor2: '#a7d0cd',
+};
